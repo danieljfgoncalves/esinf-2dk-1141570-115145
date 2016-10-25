cf66ae72b5bf8187f0dc3a62ee34f943f542763c
EricAmaral <1141570@isep.ipp.pt>
2016-10-25 17:54:19 +0100

    Creates main package and initializes SocialNetwork class

---
+++ src/esinf_2dk_1141570_1151452/SocialNetwork.java
@@ -0,0 +1,15 @@
+package esinf_2dk_1141570_1151452;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eric Jorge Schipper Ferreira do Amaral 1141570@isep.ipp.pt TurmaDN
+ */
+public class SocialNetwork {
+    
+}
