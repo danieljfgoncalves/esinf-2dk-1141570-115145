603a39fd89cc4109f03cac42ebe76dd82b3e3681
EricAmaral <1141570@isep.ipp.pt>
2016-10-27 02:03:17 +0100

    Implements City and SocialNetwork classes [implementation]

--- src/esinf_2dk_1141570_1151452/City.java
+++ src/esinf_2dk_1141570_1151452/City.java
@@ -1,11 +1,189 @@
 package esinf_2dk_1141570_1151452;
 
+import javafx.util.Pair;
+
 /**
  * Represents a City
- * 
+ *
  * @author Daniel Gonçalves 1151452@isep.ipp.pt
  * @author Eric Jorge Schipper Ferreira do Amaral 1141570@isep.ipp.pt
  */
 public class City {
-    
+    //INSTANCE ATTRIBUTES
+
+    /**
+     * City's name
+     */
+    private String name;
+
+    /**
+     * City's points
+     */
+    private int points;
+
+    /**
+     * City's coordinates
+     */
+    private Pair<Double, Double> coordinates;
+
+    /**
+     * City's mayor
+     */
+    private User mayor;
+
+    //CLASS ATTRIBUTES
+    //CONSTANTS
+    /**
+     * City's name by default
+     */
+    private static final String DEFAULT_NAME = "undefined";
+
+    /**
+     * City's points by default
+     */
+    private static final int DEFAULT_POINTS = 0;
+
+    /**
+     * City's pair by default
+     */
+    private static final Pair<Double, Double> DEFAULT_COORDINATES = new Pair(new Double(0), new Double(0));
+
+    //CONSTRUCTORS
+    /**
+     * builds up an instance of City with parameters by default
+     */
+    public City() {
+        this.coordinates = DEFAULT_COORDINATES;
+        this.mayor = new User();
+        this.name = DEFAULT_NAME;
+        this.points = DEFAULT_POINTS;
+    }
+
+    /**
+     * builds up an instance of City from another instance of City
+     *
+     * @param otherCity
+     */
+    public City(City otherCity) {
+        this.coordinates = otherCity.coordinates;
+        this.mayor = otherCity.mayor;
+        this.name = otherCity.name;
+        this.points = otherCity.points;
+    }
+
+    /**
+     * builds up an instance of City with parameters coordinates, mayor, name, points
+     * 
+     * @param coordinates
+     * @param mayor
+     * @param name
+     * @param points
+     */
+    public City(Pair coordinates, User mayor, String name, int points) {
+        this.coordinates = coordinates;
+        this.mayor = mayor;
+        this.name = name;
+        this.points = points;
+    }
+
+    //GETTERS AND SETTERS
+    /**
+     * Obtain the City's name
+     *
+     * @return the City's name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Modifies the City's name
+     *
+     * @param name the City's name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Obtain the City's points
+     *
+     * @return the City's points
+     */
+    public int getPoints() {
+        return this.points;
+    }
+
+    /**
+     * Modifies the City's points
+     *
+     * @param points the City's points to set
+     */
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    /**
+     * Obtain the City's coordinates
+     *
+     * @return the City's coordinates
+     */
+    public Pair<Double, Double> getCoordinates() {
+        return this.coordinates;
+    }
+
+    /**
+     * Modifies the City's coordinates
+     *
+     * @param coordinates the City's coordinates to set
+     */
+    public void setCoordinates(Pair<Double, Double> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * Obtain the City's mayor
+     *
+     * @return the City's mayor
+     */
+    public User getMayor() {
+        return this.mayor;
+    }
+
+    /**
+     * Modifies the City's mayor
+     *
+     * @param mayor the City's mayor to set
+     */
+    public void setMayor(User mayor) {
+        this.mayor = mayor;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) {
+            return true;
+        }
+        if (otherObject == null || getClass() != otherObject.getClass()) {
+            return false;
+        }
+
+        final City otherCity = (City) otherObject;
+
+        return this.getName().equals(otherCity.getName())
+                && this.getPoints() == (otherCity.getPoints())
+                && this.getMayor().equals(otherCity.getMayor())
+                && this.coordinates.equals(otherCity.getCoordinates());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%nCity's name: %s %nCity's points: %d %nCity's Mayor: %s %nCity's coordinates: (%.2f, .2f)",
+                this.getName(),
+                this.getPoints(),
+                this.getMayor(),
+                this.getCoordinates().getKey(),
+                this.getCoordinates().getValue());
+    }
+
 }
--- src/esinf_2dk_1141570_1151452/SocialNetwork.java
+++ src/esinf_2dk_1141570_1151452/SocialNetwork.java
@@ -1,11 +1,125 @@
 package esinf_2dk_1141570_1151452;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Represents a SocialNetwork
- * 
+ *
  * @author Daniel Gonçalves 1151452@isep.ipp.pt
  * @author Eric Jorge Schipper Ferreira do Amaral 1141570@isep.ipp.pt
  */
 public class SocialNetwork {
-    
+    //INSTANCE ATTRIBUTES
+
+    /**
+     * SocialNetwork's users list
+     */
+    private Set<User> usersList;
+
+    /**
+     * SocialNetwork's cities list
+     */
+    private Set<City> citiesList;
+
+    //CONSTRUCTORS
+    /**
+     * builds up an instance of SocialNetwork with parameters by default
+     */
+    public SocialNetwork() {
+        this.usersList = new HashSet<>();
+        this.citiesList = new HashSet<>();
+    }
+
+    /**
+     * builds up an instance of SocialNetwork from another instance of
+     * SocialNetwork
+     *
+     * @param otherSN
+     */
+    public SocialNetwork(SocialNetwork otherSN) {
+        this.citiesList = otherSN.citiesList;
+        this.usersList = otherSN.usersList;
+    }
+
+    /**
+     * builds up an instance of SocialNetwork with parameters usersList,
+     * citiesList
+     *
+     * @param usersList
+     * @param citiesList
+     */
+    public SocialNetwork(Set usersList, Set citiesList) {
+        this.usersList = usersList;
+        this.citiesList = citiesList;
+    }
+
+    //GETTERS AND SETTERS
+    /**
+     * Obtain the SocialNetwork's usersList
+     *
+     * @return the SocialNetwork's usersList
+     */
+    public Set<User> getUsersList() {
+        return this.usersList;
+    }
+
+    /**
+     * Modifies the SocialNetwork's usersList
+     *
+     * @param usersList the SocialNetwork's usersList to set
+     */
+    public void setUsersList(Set<User> usersList) {
+        this.usersList = usersList;
+    }
+
+    /**
+     * Obtain the SocialNetwork's citiesList
+     *
+     * @return the SocialNetwork's citiesList
+     */
+    public Set<City> getCitiesList() {
+        return this.citiesList;
+    }
+
+    /**
+     * Modifies the SocialNetwork's citiesList
+     *
+     * @param citiesList the SocialNetwork's citiesList to set
+     */
+    public void setCitiesList(Set<City> citiesList) {
+        this.citiesList = citiesList;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) {
+            return true;
+        }
+        if (otherObject == null || getClass() != otherObject.getClass()) {
+            return false;
+        }
+
+        final SocialNetwork otherSocialNetwork = (SocialNetwork) otherObject;
+
+        return this.getCitiesList().equals(otherSocialNetwork.getCitiesList())
+                && this.getUsersList().equals(otherSocialNetwork.getUsersList());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("UsersList{");
+        for (User user : this.usersList) {
+            s.append(String.format("%s%n", user));
+        }
+        s.append("}");
+        s.append("CitiesList{");
+        for (City city : this.citiesList) {
+            s.append(String.format("%s%n", city.getName()));
+        }
+        s.append("}");
+        return s.toString();
+    }
+
 }
